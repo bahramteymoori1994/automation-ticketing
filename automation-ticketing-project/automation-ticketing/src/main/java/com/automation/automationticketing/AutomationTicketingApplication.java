4944003d07c923053f05cbc46221d735e8b2386d
Bahram Teymoori <bahram.teymoori@outlook.com>
2025-05-30 21:44:12 +0330

    First init commit

---
+++ automation-ticketing-project/automation-ticketing/src/main/java/com/automation/automationticketing/AutomationTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.automation.automationticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationTicketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationTicketingApplication.class, args);
+    }
+
+}
---
+++ automation-ticketing-project/automation-ticketing/src/test/java/com/automation/automationticketing/AutomationTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automation.automationticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationTicketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
